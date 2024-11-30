57e56f5174cf577bb0bea1ba79139b73576ad5ba
mp3skater <mp3skaterr@gmail.com>
2024-11-30 16:38:27 +0100

    added standart dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ChakraProvider, theme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+const styles = {
+  global:(props) => ({
+    body:{
+      bg:mode("grey.100", "#000")(props),
+      color:mode("grey.800", "whiteAlpha.900")(props)
+    }
+  })
+}
 
 const config = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 }
+
+const theme = extendTheme({ config, styles })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
