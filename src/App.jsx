a28bf1870b0e234c0d62e21cde71b7da38b88e5e
mp3skater <mp3skaterr@gmail.com>
2024-12-02 16:20:22 +0100

    typo fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Button } from '@chakra-ui/react'
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
-import AuthPage from './pages/AuthPage/Authpage'
+import AuthPage from './pages/AuthPage/AuthPage'
 
 function App() {
   return (
